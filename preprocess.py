b2e8138610c6a7b258c6ca0f387bac45b7fd55c2
paramm <paramm@sfu.ca>
2021-11-30 15:52:16 -0800

    Data loads into DF

---
+++ preprocess.py
@@ -0,0 +1,14 @@
+import sys
+import pandas as pd
+import zipfile
+
+#adpated from https://stackoverflow.com/questions/56786321/read-multiple-csv-files-zipped-in-one-file
+filename = sys.argv[1]
+zf = zipfile.ZipFile(filename)
+
+dfs = [pd.read_csv(zf.open(f)) for f in zf.namelist()[1:]]
+df = pd.concat(dfs,ignore_index=True)
+# print(df)
+
+
+#takes 2m08
