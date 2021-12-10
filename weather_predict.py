2ddf91c932942814769cd7d421f11fa4759ac537
Jonathan <jperalgo@sfu.ca>
2021-12-10 05:12:35 -0800

    Converted all units necessary

--- weather_predict.py
+++ weather_predict.py
@@ -18,7 +18,7 @@ weather_schema = types.StructType([
     types.StructField('LONGITUDE', types.DoubleType()),
     types.StructField('ELEVATION', types.DoubleType()),
     types.StructField('DATE', types.StringType()),
-    types.StructField('DP01', types.IntegerType()),
+    types.StructField('DP01', types.IntegerType(), True),
     types.StructField('DSND', types.IntegerType()),
     types.StructField('DSNW', types.IntegerType()),
     types.StructField('DT00', types.IntegerType()),
@@ -71,11 +71,53 @@ def main(in_directory):
         att['TMAX'],
         att['TMIN'],
     )
+    df_cache = df.cache()
 
-    #TODO: Drop rows with null values
-    # df = df.filter(df.DP01.startswith('null'))
-    df.show(20);return
-    # cachedDF = df.cache()
+    #Convert units (inch -> mm, °F -> °C)
+    df_cache = df_cache.withColumn("EMNT_CONVERTED", udf_f_to_c(df_cache['EMNT']))
+    df_cache = df_cache.withColumn("EMSD_CONVERTED", udf_in_to_mm(df_cache['EMSD']))
+    df_cache = df_cache.withColumn("EMSN_CONVERTED", udf_in_to_mm(df_cache['EMSN']))
+    df_cache = df_cache.withColumn("EMXP_CONVERTED", udf_in_to_mm(df_cache['EMXP']))
+    df_cache = df_cache.withColumn("EMXT_CONVERTED", udf_f_to_c(df_cache['EMXT']))
+    df_cache = df_cache.withColumn("EVAP_CONVERTED", udf_in_to_mm(df_cache['EVAP']))
+    df_cache = df_cache.withColumn("PRCP_CONVERTED", udf_in_to_mm(df_cache['PRCP']))
+    df_cache = df_cache.withColumn("SNOW_CONVERTED", udf_in_to_mm(df_cache['SNOW']))
+    df_cache = df_cache.withColumn("TAVG_CONVERTED", udf_f_to_c(df_cache['TAVG']))
+    df_cache = df_cache.withColumn("TMAX_CONVERTED", udf_f_to_c(df_cache['TMAX']))
+    df_cache = df_cache.withColumn("TMIN_CONVERTED", udf_f_to_c(df_cache['TMIN']))
+
+
+    df_converted = df_cache.select(
+        df_cache['STATION'],
+        df_cache['STATION_NAME'],
+        df_cache['LATITUDE'],
+        df_cache['LONGITUDE'],
+        df_cache['ELEVATION'],
+        df_cache['DATE'],
+        df_cache['DP01'],
+        df_cache['DSND'],
+        df_cache['DSNW'],
+        df_cache['DT00'],
+        df_cache['DT32'],
+        df_cache['DX70'],
+        df_cache['DX90'],
+        df_cache['DYFG'],
+        df_cache['DYTS'],
+        df_cache['EMNT_CONVERTED'].alias('EMNT'),
+        df_cache['EMSD_CONVERTED'].alias('EMSD'),
+        df_cache['EMSN_CONVERTED'].alias('EMSN'),
+        df_cache['EMXP_CONVERTED'].alias('EMXP'),
+        df_cache['EMXT_CONVERTED'].alias('EMXT'),
+        df_cache['EVAP_CONVERTED'].alias('EVAP'),
+        df_cache['PRCP_CONVERTED'].alias('PRCP'),
+        df_cache['SNOW_CONVERTED'].alias('SNOW'),
+        df_cache['TAVG_CONVERTED'].alias('TAVG'),
+        df_cache['TMAX_CONVERTED'].alias('TMAX'),
+        df_cache['TMIN_CONVERTED'].alias('TMIN'),
+    )
+    spark.catalog.clearCache()
+
+    # TODO: Drop rows with null values
 
     # #Setting Latitude
     df = df.filter(df.LATITUDE >= 48.9)
@@ -85,9 +127,20 @@ def main(in_directory):
     df = df.filter(df.LONGITUDE >= -123.3)
     df = df.filter(df.LONGITUDE <= -121.81)
 
+def f_to_c(input):
+    if(input is None):
+        return None
+    else:
+        return round((input - 32) * 5 / 9, 4)
 
+def in_to_mm(input):
+    if(input is None):
+        return None
+    else:
+        return round(input * 25.4, 4)
 
-    # df.show(); return
+udf_f_to_c = functions.udf(f_to_c, returnType=types.DoubleType())
+udf_in_to_mm = functions.udf(in_to_mm, returnType=types.DoubleType())
 
 if __name__ == '__main__':
     in_directory = sys.argv[1]
