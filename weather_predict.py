a4bf499c891dcadad55eb1a4fdf11d00b31083cd
Jonathan <jperalgo@sfu.ca>
2021-12-10 03:46:29 -0800

    ✅Added schema for dataframe

--- weather_predict.py
+++ weather_predict.py
@@ -11,9 +11,38 @@ spark.sparkContext.setLogLevel('WARN')
 assert sys.version_info >= (3, 5) # make sure we have Python 3.5+
 assert spark.version >= '2.3' # make sure we have Spark 2.3+
 
+weather_schema = types.StructType([
+    types.StructField('STATION', types.StringType()),
+    types.StructField('NAME', types.StringType()),
+    types.StructField('LATITUDE', types.DoubleType()),
+    types.StructField('LONGITUDE', types.DoubleType()),
+    types.StructField('ELEVATION', types.DoubleType()),
+    types.StructField('DATE', types.StringType()),
+    types.StructField('DP01', types.IntegerType()),
+    types.StructField('DSND', types.IntegerType()),
+    types.StructField('DSNW', types.IntegerType()),
+    types.StructField('DT00', types.IntegerType()),
+    types.StructField('DT32', types.IntegerType()),
+    types.StructField('DX70', types.IntegerType()),
+    types.StructField('DX90', types.IntegerType()),
+    types.StructField('DYFG', types.IntegerType()),
+    types.StructField('DYTS', types.IntegerType()),
+    types.StructField('EMNT', types.DoubleType()),
+    types.StructField('EMSD', types.DoubleType()),
+    types.StructField('EMSN', types.DoubleType()),
+    types.StructField('EMXP', types.DoubleType()),
+    types.StructField('EMXT', types.DoubleType()),
+    types.StructField('EVAP', types.DoubleType()),
+    types.StructField('PRCP', types.DoubleType()),
+    types.StructField('SNOW', types.IntegerType()),
+    types.StructField('TAVG', types.DoubleType()),
+    types.StructField('TMAX', types.DoubleType()),
+    types.StructField('TMIN', types.DoubleType()),
+])
+
 
 def main(in_directory):
-    att = spark.read.option("header", "true").csv(in_directory)
+    att = spark.read.option("header", "true").csv(in_directory, schema=weather_schema)
     df = att.select(
         att['STATION'],
         att['NAME'].alias("STATION_NAME"),
@@ -21,7 +50,6 @@ def main(in_directory):
         att['LONGITUDE'],
         att['ELEVATION'],
         att['DATE'],
-        att['CLDD'],
         att['DP01'],
         att['DSND'],
         att['DSNW'],
@@ -37,32 +65,27 @@ def main(in_directory):
         att['EMXP'],
         att['EMXT'],
         att['EVAP'],
-        att['HTDD'],
-        att['HN01'],
-        att['HX01'],
-        att['LN01'],
-        att['LX01'],
-        att['MN01'],
-        att['MX01'],
         att['PRCP'],
         att['SNOW'],
         att['TAVG'],
         att['TMAX'],
         att['TMIN'],
     )
-    # df = df.cache()
 
-    # #Setting Latitde
+    #TODO: Drop rows with null values
+    # df = df.filter(df.DP01.startswith('null'))
+    df.show(20);return
+    # cachedDF = df.cache()
+
+    # #Setting Latitude
     df = df.filter(df.LATITUDE >= 48.9)
     df = df.filter(df.LATITUDE <= 49.43)
     #
     # #Setting Longitude
     df = df.filter(df.LONGITUDE >= -123.3)
-    # df = df.filter(df.LONGITUDE <= -121.81)
+    df = df.filter(df.LONGITUDE <= -121.81)
 
-    #Dropping columns with null values
-    df = df.dropna()
-    df.show(2000)
+
 
     # df.show(); return
 
